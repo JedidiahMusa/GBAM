91a04cd8f97a7f7e1a6244d895560742379a4541
JedidiahMusa <jedidiahsimon3@gmail.com>
2025-09-27 06:10:04 +0100

    .

--- src/components/dashboard/Colaborations.tsx
+++ src/components/dashboard/Colaborations.tsx
@@ -64,7 +64,7 @@ function Colaborations() {
         </div>
 
         {/* Three carousels */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {[group1, group2, group3].map((group, idx) => (
             <div
               key={idx}
